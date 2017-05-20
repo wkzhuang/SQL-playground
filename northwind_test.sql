82ec4777f6bb2a1f3559ab0cd1446abe8e98fac7
James Zhuang <jameszwk9596@gmail.com>
2017-05-20 12:58:00 -0700

    Add files via upload

--- northwind_test.sql
+++ northwind_test.sql
@@ -14,5 +14,17 @@ FROM orders
 GROUP by ship_state_province
 ORDER by sum(shipping_fee) DESC;
 
---- Part 3
+-- Part 3
+-- https://www.w3schools.com/SQL/trysql.asp?filename=trysql_select_all
+SELECT Products.ProductName,
+Suppliers.SupplierName, Suppliers.Phone 
+FROM Products
+INNER JOIN Suppliers
+ON Products.SupplierID = Suppliers.SupplierID;
 
+-- 3.2
+SELECT Suppliers.SupplierName, Suppliers.City, Suppliers.Country, avg(Products.Price)
+FROM Suppliers
+INNER JOIN Products
+ON Products.SupplierID = Suppliers.SupplierID
+GROUP BY Suppliers.SupplierName; -- use group by or avg will return one average price for the whole table
